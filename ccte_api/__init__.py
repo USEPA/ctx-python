d30dc8b9cdcbd0cc540283e0f4d156b38aa93bab
kaphillips <phillips.katherine@epa.gov>
2024-05-29 11:45:12 -0400

    cleaned up nested ifs in command_line

--- ccte_api/__init__.py
+++ ccte_api/__init__.py
@@ -9,7 +9,7 @@ if not version_info >= (3,10):
     raise RuntimeError("`ccte_api` needs Python 3.10 or higher.")
 
 print("""
-This software/application was developed by the U.S. Environmental Protection Agency 
+`ccte_api` was developed by the U.S. Environmental Protection Agency 
 (USEPA). No warranty expressed or implied is made regarding the accuracy or utility 
 of the system, nor shall the act of distribution constitute any such warranty. The 
 USEPA has relinquished control of the information and no longer has responsibility 
@@ -18,4 +18,5 @@ reference to specific commercial products, processes, or services by service mar
 trademark, manufacturer, or otherwise, does not constitute or imply their endorsement, 
 recommendation or favoring by the USEPA. The USEPA seal and logo shall not be used in 
 any manner to imply endorsement of any commercial product or activity by the USEPA or 
-the United States Government.""")
+the United States Government.
+""")
--- ccte_api/command_line.py
+++ ccte_api/command_line.py
@@ -28,53 +28,31 @@ def init(x_api_key: str, source: str = "public"):
         }
     }
 
-    path = Path.home() / ".config"
-
-    ## There is ~/.config/
-    if path.is_dir():
-        path = path.joinpath("ccte_api")
-
-        ## There is ~/.config/ccte_api/
-        if path.is_dir():
-            path = path.joinpath("config.toml")
-
-            ## There is ~/.config/ccte_api/config.toml
-            if path.is_file():
-                ## Read file and if there is a public_ccte_api table
-                data = toml.load(path)
-
-                if f"{source}_ccte_api" in data.keys():
-                    raise TableExistsError(
-                        f"Table for`{source}_ccte_api` already exists in "
-                        f"{path.as_posix()}, you can manually change the API key, "
-                        "if needed."
-                    )
-                else:
-                    with path.open("a") as f:
-                        toml.dump(data, f)
-
-            ## There is NOT ~/.config/ccte_api/config.toml
-            else:
-                with path.open("w") as f:
-                    toml.dump(data, f)
-
-        ## There is NOT ~/.config/ccte_api/
-        else:
-            path = path.joinpath("config.toml")
+    path = Path.home() / ".config" / "ccte_api" / "config.toml"
+    
+    if not path.is_file():
+        
+        if not path.parent.is_dir():
             path.parent.mkdir(parents=True, exist_ok=False)
-
-            with path.open("w") as f:
-                toml.dump(data, f)
-
-    ## There is NOT a .config/ directory in the home directory
-    else:
-        path = path.joinpath("ccte_api").joinpath("config.toml")
-        path.parent.mkdir(parents=True, exist_ok=False)
-
+            
         with path.open("w") as f:
             toml.dump(data, f)
 
+    else:
+        data = toml.load(path)
+
+        if f"{source}_ccte_api" in data.keys():
+            raise TableExistsError(
+                f"Table for`{source}_ccte_api` already exists in "
+                f"{path.as_posix()}, you can manually change the API key, "
+                "if needed."
+            )
+        else:
+            with path.open("a") as f:
+                toml.dump(data, f)
+
     return
+
 
 
 def main():
