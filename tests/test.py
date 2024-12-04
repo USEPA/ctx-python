7feafdbb2392f632b4bfafa2f46a7be710b24a57
kaphillips <phillips.katherine@epa.gov>
2024-12-04 16:44:26 -0500

    added cheminformatics test to test suite

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,11 @@ import unittest
 from chemical_test import TestChemical
 from exposure_test import TestExposure
 from hazard_test import TestHazard
+from cheminformatics_test import TestCheminformatics
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite([loader.loadTestsFromTestCase(TestChemical),
+                            loader.loadTestsFromTestCase(TestCheminformatics),
                             loader.loadTestsFromTestCase(TestExposure),
                             loader.loadTestsFromTestCase(TestHazard)])
 runner = unittest.TextTestRunner()
